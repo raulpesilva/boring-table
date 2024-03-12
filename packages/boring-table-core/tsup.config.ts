f5d3b177806e90d059f877c95e5c9d8ac2140514
raulpesilva <adraulpesilva@gmail.com>
2024-03-12 15:09:30 -0300

    chore: update build

--- packages/boring-table-core/tsup.config.ts
+++ packages/boring-table-core/tsup.config.ts
@@ -2,12 +2,13 @@ import { defineConfig } from 'tsup';
 
 export default defineConfig({
   entryPoints: ['src/*.ts'],
-  format: ['cjs', 'esm'],
+  format: ['cjs', 'esm', 'iife'],
   dts: true,
   sourcemap: true,
   clean: true,
   splitting: true,
   treeshake: true,
   shims: true,
+  legacyOutput: true,
   ignoreWatch: ['**/node_modules/**', '**/.git/**', '**/dist/**', '**/build/**', '**/coverage/**'],
 });
