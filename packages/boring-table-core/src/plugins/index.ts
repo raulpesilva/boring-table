e00c63f7999c1b3c70314e89eb08863c7c472687
raulpesilva <adraulpesilva@gmail.com>
2024-03-18 18:48:17 -0300

    chore: export order

--- packages/boring-table-core/src/plugins/index.ts
+++ packages/boring-table-core/src/plugins/index.ts
@@ -1,8 +1,9 @@
+export { BoringPlugin } from './base';
+export type { BASE_PRIORITIES, IBoringPlugin } from './base';
+
 export * from './Fetch';
 export * from './RowHidden';
 export * from './RowSelection';
-export { BoringPlugin } from './base';
-export type { BASE_PRIORITIES, IBoringPlugin } from './base';
 export * from './change';
 export * from './filter';
 export * from './pagination';
